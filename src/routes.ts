7d8b02ee47f4db7a472f15af69f7aa21d5430d05
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2021-02-26 19:30:13 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -14,9 +14,24 @@ router.get('/api/category/create/:name', async (req, res) => {
     name: req.params.name,
   })
     .then(() => {
-      res.status(200).json({ message: 'Object created' })
+      res.status(200).json({ message: 'Category created' })
     })
     .catch(() => {
+      res.status(400).json({ message: 'Error' })
+    })
+})
+
+router.get('/api/category/delete/:id', async (req, res) => {
+  Models.Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => {
+      res.status(200).json({ message: 'Category deleted' })
+    })
+    .catch((error) => {
+      console.log(error)
       res.status(400).json({ message: 'Error' })
     })
 })
